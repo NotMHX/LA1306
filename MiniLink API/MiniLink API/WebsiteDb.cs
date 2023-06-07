b1e252c9fc6b9763c69a0e777e07066487c0de28
LucaDalCorso2004 <L.DalCorso.inf21@stud.bbbaden.ch>
2023-06-07 08:05:18 +0200

    klassenmodel erstellt

---
+++ MiniLink API/MiniLink API/WebsiteDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MiniLink_API
+{
+    public class WebsiteDb : DbContext
+    {
+        public WebsiteDb(DbContextOptions<WebsiteDb> options) : base(options)
+        {
+        }
+        public DbSet<Website> Websites { get; set; }
+    }
+}
